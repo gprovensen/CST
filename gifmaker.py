3db76c67f7474bdfcf003eafd93b7c5a15ffbdf0
riley <rperalta14@smpanthers.org>
2015-03-05 17:32:40 -0800

    begin

--- gifmaker.py
+++ gifmaker.py
@@ -1,7 +1,6 @@
 def gifmaker():
-    from moviepy.editor import *
     vid = (VideoFileClip()
-        .subclip((1,22.65),(1,23.2))
-        .resize(0.5))
-        .crop(x1=150,x2=400)) 
+        subclip((1,22.65),(1,23.2))
+        resize(0.5))
+        crop(x1=150,x2=400))
     clip.write_gif("project.gif")
