6b89a30c1844a26c7e7538630fed4635e61e5ea5
riley <rperalta14@smpanthers.org>
2015-03-11 20:45:28 -0700

    fix
    no errors should work

--- gifmaker.py
+++ gifmaker.py
@@ -1,6 +1,9 @@
-def gifmaker():
-    vid = (VideoFileClip()
-        subclip((1,22.65),(1,23.2))
-        resize(0.5))
-        crop(x1=150,x2=400))
+def gifmake():
+    chs = pickAFile()
+    vid = (VideoFileClip(chs))
+    for f in vid:
+        subclip((0, 3.5), (0, 4))
+        resize(0.5)
+        crop(x1=150, x2=400)
+        print f
     clip.write_gif("project.gif")
